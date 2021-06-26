11d7916a8a89bf819a26d1c5a0f3f69362b3219a
Aniruddha770 <patilanirudha38@gmail.com>
2021-06-26 13:12:24 +0530

    Test case1 Added

---
+++ TU_MaxFinder.java
@@ -0,0 +1,27 @@
+import com.aniruddha.max.MaxFinder;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TU_MaxFinder {
+    @Test
+    void WhenIntegerShouldReturnMax() {
+        Integer int1=1,int2=4,int3=3;
+        Integer maxnumber =  MaxFinder.findMax(int1,int2,int3);
+        Assertions.assertEquals(4,maxnumber);
+    }
+
+    @Test
+    void WhenIntegerShouldReturnMax1() {
+        Integer int1=4,int2=3,int3=1;
+        Integer maxnumber =  MaxFinder.findMax(int1,int2,int3);
+        Assertions.assertEquals(4,maxnumber);
+    }
+
+    @Test
+    void WhenIntegerShouldReturnMax2() {
+        Integer int1=3,int2=1,int3=4;
+        Integer maxnumber =  MaxFinder.findMax(int1,int2,int3);
+        Assertions.assertEquals(4,maxnumber);
+    }
+
+}
