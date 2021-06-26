e37dd8754007a22334599e3551a2e30caedb9caa
Aniruddha770 <patilanirudha38@gmail.com>
2021-06-26 13:23:37 +0530

    UC2 Added

--- MaxFinder.java
+++ MaxFinder.java
@@ -8,8 +8,9 @@
 package com.aniruddha.max;
 
 public class MaxFinder {
-    public static Integer findMax(Integer number1, Integer number2, Integer number3) {
-        Integer max= number1;
+
+    public static Float findMax1(Float number1, Float number2, Float number3) {
+        Float max= number1;
         if (max < number2){
             max=number2;
         }
